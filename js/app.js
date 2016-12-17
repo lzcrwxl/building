121697dcbc725915b7d6cf60c03d68923b351431
wxl <lzcrwxl@163.com>
2016-12-17 17:13:08 +0800

    version 3.0

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by lx on 2016/12/8.
  */
+$(function(){
+  $.fn.switch=function(){
+    $(this).on('click','li',function(){
+      $(this).addClass('current').siblings('.current').removeClass();
+    })
+    return $(this);
+  };
+  //左侧切图
+
+  /*$('.subSideList-2').on('click','li',function(){
+    $(this).addClass('current').siblings('.current').removeClass('current');
+  })*/
+  $('.subSideList-2').switch();
+  $('.subSideList').switch();
+});
+
