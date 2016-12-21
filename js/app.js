fba02f3a26b651eb60de5b02e5c0c2cb9ee40feb
wxl <lzcrwxl@163.com>
2016-12-21 08:50:02 +0800

    updated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /**
  * Created by lx on 2016/12/8.
  */
-$(function(){
-  $.fn.switch=function(){
-    $(this).on('click','li',function(){
+$(function () {
+  $.fn.switch = function () {
+    $(this).on('click', 'li', function () {
       $(this).addClass('current').siblings('.current').removeClass();
     })
     return $(this);
@@ -11,9 +11,12 @@ $(function(){
   //左侧切图
 
   /*$('.subSideList-2').on('click','li',function(){
-    $(this).addClass('current').siblings('.current').removeClass('current');
-  })*/
+   $(this).addClass('current').siblings('.current').removeClass('current');
+   })*/
   $('.subSideList-2').switch();
   $('.subSideList').switch();
-});
+  $('.chkStyle').click(function () {
+    $(this).toggleClass('backgroundshow');
+  })
+})
 
